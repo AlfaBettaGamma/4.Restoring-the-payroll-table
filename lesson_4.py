3af4210624a6f0defe1b1460f886c796c9af1cea
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-05-24 12:28:55 +0300

    update

--- lesson_4.py
+++ lesson_4.py
@@ -1,13 +1,15 @@
 def  SynchronizingTables(N, ids, salary):
   new_ids = []
+  new_salary = []
   if len(ids) == len(salary) == N:
     for i in range(len(ids)):
       new_ids.append(ids[i])
-    for i in range(len(salary) - 1):
-      for j in range(len(salary) - i - 1):
+      new_salary.append(new_salary[i])
+    for i in range(len(new_salary) - 1):
+      for j in range(len(new_salary) - i - 1):
         if new_ids[j] < new_ids[j + 1] :
           new_ids[j], new_ids[j + 1] = new_ids[j + 1], new_ids[j]
-          salary[j], salary[j + 1] = salary[j + 1], salary[j]
-    return salary
+          new_salary[j], new_salary[j + 1] = new_salary[j + 1], new_salary[j]
+    return new_salary
   else:
     return print("Неверное значение!")
